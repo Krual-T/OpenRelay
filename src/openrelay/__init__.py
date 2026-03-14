2b0f47660bbd22071637f52038fe9a60fc0011bf
Shaokun.Tang <shaokun.tang@outlook.com>
2026-03-15 00:12:23 +0800

    Trim root package exports after package refactor

--- src/openrelay/__init__.py
+++ src/openrelay/__init__.py
@@ -1,31 +1,5 @@
 __version__ = "0.1.0"
-from .core import (
-    ActiveRun,
-    AppConfig,
-    BackendConfig,
-    BackendReply,
-    ConfigError,
-    DirectoryShortcut,
-    FeishuConfig,
-    IncomingMessage,
-    SessionRecord,
-    SessionSummary,
-    load_config,
-)
-from .storage import StateStore
 
 __all__ = [
     "__version__",
-    "ActiveRun",
-    "AppConfig",
-    "BackendConfig",
-    "BackendReply",
-    "ConfigError",
-    "DirectoryShortcut",
-    "FeishuConfig",
-    "IncomingMessage",
-    "SessionRecord",
-    "SessionSummary",
-    "StateStore",
-    "load_config",
 ]
